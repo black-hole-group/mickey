b8586001b4eed02d283890b6d97449679beb1a5a
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2015-07-01 11:40:22 -0300

    reordered code; updated readme

--- README.md
+++ README.md
@@ -2,3 +2,44 @@ PLUTO Tools
 ============
 
 Assorted methods and classes to handle and visualize the output of the PLUTO MHD code.
+	
+To read the simulation output for snapshot 10 (e.g. rho.0010.dbl etc):
+	
+```python 
+import pluto
+c=pluto.Pluto(10)
+```
+
+The *p* object's attributes are now:
+	
+- grid: x1,x2,x3
+- velocities: v1,v2,v3
+- pressure p
+- density rho
+- number of grid cells: n1,n2,n3
+
+
+Plots density field in cartesian coordinates:
+
+```python	
+c.snap()
+```
+
+or if you don't want to define an object:
+
+```python	
+pluto.Pluto(10).snap()
+```
+
+Plots a density field for snapshot 50 which was generated in polar coordinates, "regridded" with a 400x400 cartesian grid:
+
+```python	
+p=pluto.Pluto(50).pol2cart(400)
+p.snap()
+```
+or
+```python	
+pluto.Pluto(50).pol2cart(400).snap()
+```
+
+More examples coming soon.
--- pluto.py
+++ pluto.py
@@ -4,7 +4,6 @@ Methods to read and visualize PLUTO's output.
 
 
 import pyPLUTO as pp
-import mayavi.mlab as mlab
 import pylab, numpy
 import nemmen
 
@@ -12,6 +11,9 @@ import nemmen
 
 
 def movie(fname="movie.avi"):
+	"""
+3D movie generation. 
+	"""
 	import fish
 	import os, fnmatch
 	import subprocess
@@ -33,7 +35,95 @@ def movie(fname="movie.avi"):
 	for i in range(0,nfiles-1):
 		#cutplane(i)
 		volume(i)
+		#snap(i)
 		peixe.animate(amount=i)
+
+
+
+
+
+
+#### 3d routines
+
+
+def cutplane(i):
+	"""
+Snapshot of 3d cartesian simulation, generating cut planes.	
+
+i : index corresponding to frame you want to plot
+	"""
+	import mayavi.mlab as mlab
+
+	d=pp.pload(i)
+	x1,x2,x3=d.x1,d.x2,d.x3
+	v1,v2,v3=d.vx1,d.vx2,d.vx3
+	p=d.prs
+	rho=d.rho
+
+	mlab.clf()
+	#mlab.figure(size=(600,600))
+
+	# volume rendering
+	mp=mlab.pipeline.scalar_field(p)
+	mrho=mlab.pipeline.scalar_field(rho)
+	#mlab.pipeline.volume(mp)#,vmax=rho.max()/5.)
+
+	# streamlines
+	#flow = mlab.flow(v1, v2, v3, seed_scale=0.5, seed_resolution=8, integration_direction='both',seed_visible=False)
+
+	# cut planes
+	mlab.pipeline.image_plane_widget(mp, plane_orientation='y_axes', slice_index=100)
+	mlab.pipeline.image_plane_widget(mp, plane_orientation='x_axes', slice_index=100)
+
+	# move camera to appropriate distance
+	dcam=mlab.view()[2] # distance of camera to center
+	##mlab.move(forward=dcam/2.)
+	mlab.view(distance=dcam/2.)
+
+	# saves snapshot
+	mlab.savefig('plot.'+str(i)+'.jpeg',size=(800,800))
+
+
+
+
+def volume(i):
+	"""
+Volume rendering of 3d cartesian simulation.	
+	
+i : index corresponding to frame you want to plot
+	"""
+	import mayavi.mlab as mlab
+
+	d=pp.pload(i)
+	x1,x2,x3=d.x1,d.x2,d.x3
+	v1,v2,v3=d.vx1,d.vx2,d.vx3
+	p=d.prs
+	rho=d.rho
+
+	mlab.clf()
+
+	# volume rendering
+	mp=mlab.pipeline.scalar_field(p)
+	mrho=mlab.pipeline.scalar_field(rho)
+	mlab.pipeline.volume(mp)#,vmax=rho.max()/5.)
+
+	# streamlines
+	#flow = mlab.flow(v1, v2, v3, seed_scale=0.5, seed_resolution=8, integration_direction='both',seed_visible=False)
+
+	# cut planes
+	#mlab.pipeline.image_plane_widget(mp, plane_orientation='y_axes', slice_index=100)
+	#mlab.pipeline.image_plane_widget(mp, plane_orientation='x_axes', slice_index=100)
+
+	# move camera to appropriate distance
+	dcam=mlab.view()[2] # distance of camera to center
+	##mlab.move(forward=dcam/2.)
+	mlab.view(distance=dcam/2.)
+
+	# saves snapshot
+	mlab.savefig('plot.'+str(i)+'.jpeg',size=(800,800))
+
+
+
 
 
 
@@ -126,6 +216,11 @@ Creates a new object with variables in cartesian coordinates.
 Useful if the object was created by PLUTO in polar coordinates.
 
 n is the new number of elements n^2.
+
+>>> import pluto
+>>> c=pluto.Pluto(100)
+>>> p=c.pol2cart(400) # creates new object in polar coordinates
+>>> p.snap() # plots in polar coords.
 		"""
 		# creates copy of current object which will have the new
 		# coordinates
@@ -155,82 +250,6 @@ n is the new number of elements n^2.
 		
 		return obj	
 
-
-
-#### 3d routines
-
-
-def cutplane(i):
-	"""
-Snapshot of 3d cartesian simulation, generating cut planes.	
-
-i : index corresponding to frame you want to plot
-	"""
-	d=pp.pload(i)
-	x1,x2,x3=d.x1,d.x2,d.x3
-	v1,v2,v3=d.vx1,d.vx2,d.vx3
-	p=d.prs
-	rho=d.rho
-
-	mlab.clf()
-	#mlab.figure(size=(600,600))
-
-	# volume rendering
-	mp=mlab.pipeline.scalar_field(p)
-	mrho=mlab.pipeline.scalar_field(rho)
-	#mlab.pipeline.volume(mp)#,vmax=rho.max()/5.)
-
-	# streamlines
-	#flow = mlab.flow(v1, v2, v3, seed_scale=0.5, seed_resolution=8, integration_direction='both',seed_visible=False)
-
-	# cut planes
-	mlab.pipeline.image_plane_widget(mp, plane_orientation='y_axes', slice_index=100)
-	mlab.pipeline.image_plane_widget(mp, plane_orientation='x_axes', slice_index=100)
-
-	# move camera to appropriate distance
-	dcam=mlab.view()[2] # distance of camera to center
-	##mlab.move(forward=dcam/2.)
-	mlab.view(distance=dcam/2.)
-
-	# saves snapshot
-	mlab.savefig('plot.'+str(i)+'.jpeg',size=(800,800))
-
-
-
-
-def volume(i):
-	"""
-Volume rendering of 3d cartesian simulation.	
-	
-i : index corresponding to frame you want to plot
-	"""
-	d=pp.pload(i)
-	x1,x2,x3=d.x1,d.x2,d.x3
-	v1,v2,v3=d.vx1,d.vx2,d.vx3
-	p=d.prs
-	rho=d.rho
-
-	mlab.clf()
-
-	# volume rendering
-	mp=mlab.pipeline.scalar_field(p)
-	mrho=mlab.pipeline.scalar_field(rho)
-	mlab.pipeline.volume(mp)#,vmax=rho.max()/5.)
-
-	# streamlines
-	#flow = mlab.flow(v1, v2, v3, seed_scale=0.5, seed_resolution=8, integration_direction='both',seed_visible=False)
-
-	# cut planes
-	#mlab.pipeline.image_plane_widget(mp, plane_orientation='y_axes', slice_index=100)
-	#mlab.pipeline.image_plane_widget(mp, plane_orientation='x_axes', slice_index=100)
-
-	# move camera to appropriate distance
-	dcam=mlab.view()[2] # distance of camera to center
-	##mlab.move(forward=dcam/2.)
-	mlab.view(distance=dcam/2.)
-
-	# saves snapshot
-	mlab.savefig('plot.'+str(i)+'.jpeg',size=(800,800))
 
 
 
