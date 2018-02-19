8edc09e3aa965a78d186afdc72b87119fa6765ef
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-02-19 17:18:13 -0300

    long list of input arrays

--- mickey/regrid.c
+++ mickey/regrid.c
@@ -1,10 +1,20 @@
-/* Need to use 1D index for accessing array elements */
-void modifyArray(int sizex, int sizey, double *arr) {
-  for (int i=0; i<sizex; i++) {
-  	for (int j=0; j<sizey; j++) {
-  		int n=i*sizey+j;
+/*
+Performs change of coordinate basis and regridding of arrays from a
+polar to cartesian basis.
 
-  		arr[n] = i*j;
-  	}
-  }
+Variables t* are temporary ones which are not required in the code, only
+for the purposes of using numpy.
+*/
+void modifyArray(int nxnew, int nynew, double *xnew, int t1, int t2, double *ynew, int nx, int ny, double *rho, int t3, int t4, double *p, int t5, int t6, double *v1, int t7, int t8, double *v2) {
+	// goes through new array
+	for (int i=0; i<nxnew; i++) {
+		for (int j=0; j<nynew; j++) {
+  			// Need to use 1D index for accessing array elements 
+			int n=i*nynew+j;
+
+			
+
+			arr[n] = i*j;
+		}
+	}
 }
