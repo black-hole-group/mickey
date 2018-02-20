8dab132f9cdd684a18c751287bfafacee03d8b9f
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-02-20 19:42:26 -0300

    modifying setup script for compiling the C extension

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 
 with open('README.md') as f:
     readme = f.read()
@@ -6,14 +6,20 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+_fastregrid = Extension("_fastregrid",
+                   ["fastregrid.i","fastregrid.c"],
+                   #include_dirs = [numpy_include],
+                   )
+
 setup(
     name='mickey',
-    version='0.1',
+    version='0.2',
     description='Assorted methods and classes to handle and visualize the output of the Pluto MHD code',
     long_description=readme,
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://bitbucket.org/nemmen/mickey',
     license=license,
+    ext_modules=[Extension('_fastregrid', ['fastregrid.c'])],
     packages=find_packages(exclude=('tests', 'docs'))
 )
