5962c5fc5094cdac0f09cc3ef246a7adb4b07d48
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-02-26 05:05:32 -0300

    incorporated pyopencl in mickey

--- mickey/mickey.py
+++ mickey/mickey.py
@@ -603,12 +603,12 @@ choice can affect some specific transformations.
 
 :param n: New number of elements n^2. If None, figures out by itself
 :param xlim: Boundary for the plot and the grid
+
+More info about this implementation on jupyter notebook "pluto debug
+regridFast".
 		"""
 		import nmmn.lsd, nmmn.misc
-
-		# C function for fast regridding. Make sure you compile it first
-		# with make
-		import fastregrid
+		import pyopencl as cl  
 
 		# creates copy of current object which will have the new
 		# coordinates
@@ -623,8 +623,6 @@ choice can affect some specific transformations.
 
 		# figures out optimal size of cartesian grid
 		if n is None:
-			#n=numpy.sqrt(self.x1.size*self.x2.size)*2	# notice the factor of 2
-			#n=int(n)
 			n=self.optimalgrid()
 
 		if(gmtry == "SPHERICAL" or gmtry == "CYLINRICAL"):
@@ -634,16 +632,79 @@ choice can affect some specific transformations.
 			xnew=numpy.linspace(-xlim, xlim, n)
 			ynew=numpy.linspace(-xlim, xlim, n)
 
+		# output arrays that will be incorporated in a new Pluto object
 		rho=numpy.zeros((n,n))
 		vx=numpy.zeros((n,n))
 		vy=numpy.zeros((n,n))
 		vz=numpy.zeros((n,n)) # vphi
 		p=rho.copy()
 
-		if(gmtry == "SPHERICAL"):
-			fastregrid.regrid(xnew, ynew, r, th, self.rho, self.p, self.v1, self.v2, self.v3, rho, p, vx, vy, vz)		
-		else: #polar case for bondi
-			print("Geometry not supported. Improve the method.")
+		# OPENCL SETUP
+		# ===========================================================
+		# gets device for GPU
+		platforms=cl.get_platforms()
+		devices=platforms[0].get_devices(cl.device_type.GPU)
+		context=cl.Context([devices[0]])
+		#context = cl.create_some_context()
+		queue = cl.CommandQueue(context)
+		mf = cl.mem_flags
+
+		# ## Host variables
+		# Define host arrays with appropriate precision, suffix `_h`. 
+		# First the input arrays
+		xnew_h = xnew.astype(np.float32)
+		ynew_h = ynew.astype(np.float32)
+		r_h = r.astype(np.float32)
+		th_h = th.astype(np.float32)
+		rhoin_h = self.rho.astype(np.float32)
+		pin_h = self.p.astype(np.float32)
+		v1in_h = self.v1.astype(np.float32)
+		v2in_h = self.v2.astype(np.float32)
+		v3in_h = self.v3.astype(np.float32)
+
+		# then the output arrays
+		rho_h = rho.astype(np.float32)
+		p_h = p.astype(np.float32)
+		vx_h = vx.astype(np.float32)
+		vy_h = vy.astype(np.float32)
+		vz_h = vz.astype(np.float32)
+
+		# ## Device variables
+		# Buffers, suffix `_d`. Input arrays
+		xnew_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=xnew_h)
+		ynew_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=ynew_h)
+		r_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=r_h)
+		th_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=th_h)
+		rhoin_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=rhoin_h)
+		pin_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pin_h)
+		v1in_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=v1in_h)
+		v2in_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=v2in_h)
+		v3in_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=v3in_h)
+
+		# Output arrays
+		rho_d = cl.Buffer(context, mf.WRITE_ONLY, rho_h.nbytes)
+		p_d = cl.Buffer(context, mf.WRITE_ONLY, rho_h.nbytes)
+		vx_d = cl.Buffer(context, mf.WRITE_ONLY, rho_h.nbytes)
+		vy_d = cl.Buffer(context, mf.WRITE_ONLY, rho_h.nbytes)
+		vz_d = cl.Buffer(context, mf.WRITE_ONLY, rho_h.nbytes)
+
+		# ## Execute kernel
+		kernel=open('fastregrid.cl').read()
+		program = cl.Program(context, kernel).build()
+
+		program.regrid(queue, rho_h.shape, None, numpy.int32(xnew.size), xnew_d, numpy.int32(ynew.size), ynew_d, numpy.int32(r.size), r_d, numpy.int32(th.size), th_d, rhoin_d, pin_d, v1in_d, v2in_d, v3in_d, rho_d, p_d, vx_d, vy_d, vz_d)
+
+		# ## Gathers output
+		cl.enqueue_copy(queue, rho_h, rho_d)
+		cl.enqueue_copy(queue, p_h, p_d)
+		cl.enqueue_copy(queue, vx_h, vx_d)
+		cl.enqueue_copy(queue, vy_h, vy_d)
+		cl.enqueue_copy(queue, vz_h, vz_d)
+
+		# ===========================================================
+		# END OF OPENCL PART
+
+
 
 		# coordinate arrays
 		obj.x1,obj.x2=xnew,ynew # cartesian coords, 1D
@@ -654,13 +715,13 @@ choice can affect some specific transformations.
 		obj.RSP, obj.THSP = numpy.meshgrid(obj.rsp,obj.thsp) # spherical polar coords, 2D
 
 		# velocities
-		obj.v1,obj.v2,obj.v3 = vx.T,vy.T,vz.T # Cartesian components
+		obj.v1,obj.v2,obj.v3 = vx_h,vy_h,vz_h # Cartesian components
 		obj.vr, obj.vth = nmmn.misc.vel_c2p(obj.TH,obj.v1,obj.v2) # polar components
 		obj.speed = numpy.sqrt(obj.v1**2+obj.v2**2+obj.v3**3)
 
 		# fluid variables
 		obj.gamma=self.gamma
-		obj.rho,obj.p=rho.T,p.T
+		obj.rho,obj.p=rho_h,p_h
 		obj.entropy=numpy.log(obj.p/obj.rho**obj.gamma)
 		obj.am=obj.v3*obj.R*numpy.sin(obj.THSP) # specific a. m., vphi*r*sin(theta)
 		obj.Be=obj.speed**2/2.+obj.gamma*obj.p/((obj.gamma-1.)*obj.rho)-1./obj.R	# Bernoulli function
